e289010f2b76ffaf29550d19058efca867633491
Sebastian Basiński <basinski.sebastian@gmail.com>
2019-05-16 11:32:25 +0200

    Add correct variables names

--- js/script.js
+++ js/script.js
@@ -3,19 +3,19 @@
   var newGameButton = document.querySelector('#newGame'),
       startGame = document.querySelector('#startGame'),
       resetGameButton = document.querySelector('#resetGame'),
-      info_output = document.querySelector('#info-output'),
-      rounds_output = document.querySelector('#rounds-output'),
-      result_player = document.querySelector('#player'),
-      player_name = document.querySelector('#player-name'),
-      result_computer = document.querySelector('#comp'),
+      infoOutput = document.querySelector('#info-output'),
+      roundsOutput = document.querySelector('#rounds-output'),
+      resultPlayer = document.querySelector('#player'),
+      playerName = document.querySelector('#player-name'),
+      resultComputer = document.querySelector('#comp'),
       playerButtons = document.querySelectorAll('.player-move'),
-      modal_winner = document.querySelector('#modal-winner'),
-      modal_start = document.querySelector('#modal-start'),
-      close_modal = document.querySelector('#close_modal'), 
+      modalWinner = document.querySelector('#modal-winner'),
+      modalStart = document.querySelector('#modal-start'),
+      modalClose = document.querySelector('#close_modal'), 
       winner = document.querySelector('#winner-output'),
       overlay = document.querySelector('.overlay'),
-      input_player,
-      input_rounds;
+      inputPlayer,
+      inputRounds;
   
   var params = {
     playerName: 'Gracz',
@@ -46,11 +46,11 @@
  // Reset all content and variables:
   function resetAll() {
       displayMsg('');
-      player_name.innerHTML = 'Gracz';
-      info_output.innerHTML = '';
-      rounds_output.innerHTML = 0;
-      result_player.innerHTML = 0;
-      result_computer.innerHTML = 0;
+      playerName.innerHTML = 'Gracz';
+      infoOutput.innerHTML = '';
+      roundsOutput.innerHTML = 0;
+      resultPlayer.innerHTML = 0;
+      resultComputer.innerHTML = 0;
       params.playerWins = 0;
       params.computerWins = 0;
       params.roundsToWin = 0;
@@ -67,17 +67,17 @@
   
   var callNewGame = function() {
     // take values from inputs
-    input_player = document.querySelector('#inputP').value;
-    input_rounds = document.querySelector('#inputR').value;
-    // attach name player to player_name
-    if(input_player !== '') {
-      player_name.innerHTML = input_player;
+    inputPlayer = document.querySelector('#inputP').value;
+    inputRounds = document.querySelector('#inputR').value;
+    // attach name player to playerName
+    if(inputPlayer !== '') {
+      playerName.innerHTML = inputPlayer;
     } 
     // attach values to params object
-    params.playerName = input_player;
-    params.roundsToWin = input_rounds;
+    params.playerName = inputPlayer;
+    params.roundsToWin = inputRounds;
     // display rounds to finish the game
-    rounds_output.innerHTML = params.roundsToWin;
+    roundsOutput.innerHTML = params.roundsToWin;
     
     if(params.stopGame === 0) {
       resetAll();
@@ -92,7 +92,7 @@
   
   function startModal() {
     // open modal box
-    modal_start.classList.add('show');
+    modalStart.classList.add('show');
     overlay.classList.add('show');
     resetAll();
   }
@@ -102,8 +102,8 @@
 
   // Show player and computer points
   function showResult() {
-    result_player.innerHTML = params.playerWins;
-    result_computer.innerHTML = params.computerWins;
+    resultPlayer.innerHTML = params.playerWins;
+    resultComputer.innerHTML = params.computerWins;
   }
   
   // Compare gained points to rounds, and decide who wins the game
@@ -158,19 +158,19 @@
   // Show results of game on the page
   function displayMsg(type) {
     if(type === 'playerWin') {
-      info_output.innerHTML = '<p>' + params.playerName + ' won the game!' + '</p>';
+      infoOutput.innerHTML = '<p>' + params.playerName + ' won the game!' + '</p>';
     } else if (type === 'compWin') {
-      info_output.innerHTML = '<p>' + 'Computer won the game!' + '</p>';
+      infoOutput.innerHTML = '<p>' + 'Computer won the game!' + '</p>';
     } else {
-      info_output.innerHTML = '<p>' + 'There is a draw! Play again!' + '</p>';
+      infoOutput.innerHTML = '<p>' + 'There is a draw! Play again!' + '</p>';
     }
   }
   
   function showModal(type) {
     overlay.classList.add('show');
-    modal_winner.classList.add('show');
+    modalWinner.classList.add('show');
     if(type === 'playerWin') {
-      if(input_player.length !== 0) {
+      if(inputPlayer.length !== 0) {
         winner.innerHTML = '<p>' + params.playerName + ' ,YOU WON THE ENTIRE GAME!' + '</p>' + '<br>';
       } else {
         winner.innerHTML = '<p>' + 'PLAYER WON THE ENTIRE GAME!' + '</p>' + '<br>';
@@ -181,12 +181,12 @@
   }
   
   function closeModal() {
-    modal_winner.classList.remove('show');
-    modal_start.classList.remove('show');
+    modalWinner.classList.remove('show');
+    modalStart.classList.remove('show');
     overlay.classList.remove('show');
   }
   
   // close modal window by click on button or dark overlay
-  close_modal.addEventListener('click', closeModal);
+  modalClose.addEventListener('click', closeModal);
   overlay.addEventListener('click', closeModal);
 })();
